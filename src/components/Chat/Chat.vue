7e7705b0d162354fe8bbe9d6f4adbb782927d37c
JaroB2002 <116017813+JaroB2002@users.noreply.github.com>
2023-11-27 21:15:02 +0100

    Chore: Add messaging, reverse order, limit to 16

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,58 +1,55 @@
 <script setup>
-    //import ref
-    import { ref, reactive, onMounted } from 'vue'; //enkel importeren wat je nodig hebt, houdt de opslag klein
-    let message = ref(''); //ref is een reactieve variabele, voor simpele variabelen als int - string - boolean, reflection = iets in het oog houden
-    let allMessages = reactive({
-        data: [],
-    }); //reactive is voor complexe variabelen als arrays en objecten
+import { ref, reactive, onMounted } from 'vue';
 
-    onMounted(async () => {
+let message = ref('');
+let allMessages = reactive({
+    data: [],
+});
+
+onMounted(async () => {
     try {
         const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/");
         const data = await response.json();
-        allMessages.data = data;
+        allMessages.data = data.reverse().slice(0, 16); // Omgekeerde volgorde en maximaal 16 berichten
     } catch (error) {
         console.error(error);
     }
-    });
+});
 
-    const sendMessage = async () => {
+const sendMessage = async () => {
     try {
-      const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text: message.value }),
-      });
+        const response = await fetch("https://lab5-p379.onrender.com/api/v1/messages/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: message.value }),
+        });
 
-      if (response.ok) {
-        const data = await response.json();
-        const newMessage = { id: data.id, text: message.value };
-        allMessages.data.push(newMessage);
-        message.value = '';
-      } else {
-        console.error('Msg failed to send');
-      }
+        if (response.ok) {
+            const data = await response.json();
+            const newMessage = { id: data.id, text: message.value };
+            allMessages.data = [newMessage, ...allMessages.data.slice(0, 15)]; // Voeg het nieuwe bericht toe en behoud maximaal 16 berichten
+            message.value = '';
+        } else {
+            console.error('Msg failed to send');
+        }
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  };
+};
 </script>
 
 <template>
   <div>
     <ul>
-        <li v-for="m in allMessages.data" :key="m.id">
+      <li v-for="m in allMessages.data" :key="m.id">
         {{ m.text }}
       </li>
     </ul>
     <div>
-        <input v-model="message" type="text" placeholder=""> 
-        <button @click="sendMessage">Send</button> 
+      <input v-model="message" type="text" placeholder=""> 
+      <button @click="sendMessage">Send</button> 
     </div>
   </div>
 </template>
-
-<style scoped>
-</style>
